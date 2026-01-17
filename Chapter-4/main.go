05b337d62d9fcf47215c5c28d5bdf26b44f2b7b7
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-01-17 20:04:28 +0400

    feat(chapter-4): complete chapter 4

--- Chapter-4/main.go
+++ Chapter-4/main.go
@@ -9,11 +9,22 @@ func main() {
 	fmt.Print("hello, ")
 	fmt.Print("life! \n")
 	fmt.Print("yay \n \n")
-
 	fmt.Println("auto new line")
 	fmt.Println("told ya")
 	fmt.Println("i am learning", whatImLearing, "and my username is", username)
 
-	// Printf (formatted strings)
-	fmt.Printf("i am learning %v and my username is %v", whatImLearing, username)
+	// Printf (formatted strings) %_ format specifier
+	fmt.Printf("i am learning %v and my username is %v \n", whatImLearing, username)
+	// adds quotes around
+	fmt.Printf("i am learning %q and my username is %q \n", whatImLearing, username)
+	// print type
+	fmt.Printf("username is type %T \n", username)
+	// print float
+	fmt.Printf("this is a float: %f \n", 23.23)
+	// limit decimal points
+	fmt.Printf("this is a float: %0.2f \n", 23.23)
+
+	// Sprintf (save version of printf)
+	var savedStr string = fmt.Sprintf("i am learning %v and my username is %v \n", whatImLearing, username)
+	fmt.Printf("saved formatted string: %v", savedStr)
 }
